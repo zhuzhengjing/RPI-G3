f479c750111918e09c7cd88be5450e3e39b1aa80
bruce.zhu <zhuzhengjing@gmail.com>
2015-11-06 21:33:44 +0800

    come on

---
+++ PCA8538/pca8538.h
@@ -0,0 +1,20 @@
+
+#ifndef _PCA8538_H_
+#define _PCA8538_H_
+
+typedef enum
+{
+    SYMBOL_AUTO,
+    SYMBOL_W,
+    SYMBOL_S,
+    SYMBOL_CITY,
+    SYMBOL_E,
+    SYMBOL_mi,
+    SYMBOL_km_m,
+    SYMBOL_km_k,
+    SYMBOL_WRENCH,
+    SYMBOL_LED,
+    SYMBOL_NUM
+}PCA8538_symbols_t;
+
+#endif /* _PCA8538_H_ */
